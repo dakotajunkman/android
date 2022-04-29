de2bbcd0cfa89f79921e594dff80b9a020ace276
Dakota Junkman <dakota.junkman@gmail.com>
2022-04-29 12:02:40 -0500

    more practice

--- BasicKotlin/app/src/main/java/com/example/basickotlin/Basics.kt
+++ BasicKotlin/app/src/main/java/com/example/basickotlin/Basics.kt
@@ -15,4 +15,48 @@ fun main() {
     println("Hello $myName")
 
     /* Above output: DakotaHello Fred */
+
+    // Char must have single quotes
+    val char = 'a'
+
+    var string = "Hello World"
+    var first = string.first()
+    var last = string.last()
+
+    println("First char: $first")
+    println("Last char: $last")
+
+
+    var season = 3
+
+    when (season) {
+        1 -> println("spring")
+        2 -> println("summer")
+        3 -> {
+            println("fall")
+            println("autumn")
+        }
+        4 -> println("winter")
+        else -> println("lolwut")
+    }
+
+    var month = 3
+
+    when (month) {
+        in 3..5 -> println("spring")
+        in 6..8 -> println("summer")
+        in 9..11 -> println("fall")
+        12, 1, 2 -> println("winter")
+        else -> println("lolwut")
+    }
+
+    for (num in 10 downTo 1) {
+        print(num)
+    }
+
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    println(add(4, 6))
 }
